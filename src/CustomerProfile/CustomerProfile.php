12a9ff11a2f7abc536049289f07416730e74ea3c
coder <usmccoder@gmail.com>
2023-11-15 15:33:22 -0700

    improve messages

--- src/CustomerProfile/CustomerProfile.php
+++ src/CustomerProfile/CustomerProfile.php
@@ -61,13 +61,10 @@ class CustomerProfile extends AuthorizeNet
 
                 $profile_id = $matches['profileId'] ?? '';
 
-                $response = (object)['status' => true, 'profile_id' => $profile_id];
-                return $response;
+                return (object)['status' => true, 'profile_id' => $profile_id];
             }
 
-            $response = (object)['status' => false, 'message' => 'Failed, To create customer profile. ' . $response->getMessages()->getMessage()[0]->getText()];
-
-            return $response;
+            throw new \Exception('Failed, To create customer profile. ' . $response->getMessages()->getMessage()[0]->getText());
         }
 
         return $response;
